48b2ab4b0f94415a7f2f884f4fd385ec87bdd5e5
christianAlvim <christianfelipialvim@gmail.com>
2024-05-03 10:33:50 -0300

    Segunda parte do projeto OK

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
+import { CategoriaService } from './categoria/service/categoria.service';
+import { CategoriaController } from './categoria/controller/categoria.controller';
 
 @Module({
   imports: [
@@ -12,12 +16,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [],
+      entities: [Categoria],
       synchronize: true
 
-    })
+    }),
+    CategoriaModule
   ],
-  controllers: [],
-  providers: [],
+  controllers: [CategoriaController],
+  providers: [CategoriaService],
 })
 export class AppModule {}
